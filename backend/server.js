c45639cf81e71f2c29e7f0eae0e1b4c0a470b5b3
Mithu12 <mosaddekalimithu123@gmail.com>
2021-07-04 23:05:39 +0600

    deployment preparation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const path =require("path");
+
+dotenv.config()
 
 const cors = require('cors')
 
@@ -44,6 +47,18 @@ io.on("connection", socket => {
   
     socket.on("disconnect", () =>console.log("Disconnected"));
   });
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('REST is active')
+    })
+}
 
 
 
